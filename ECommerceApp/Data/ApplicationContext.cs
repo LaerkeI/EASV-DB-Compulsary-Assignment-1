4d328dbb29a834dd061fd055a0305221f216ce47
LaerkeI <imeland3@gmail.com>
2024-10-09 08:59:52 +0200

    Part 2 Task 2: Migrate to Categories

---
+++ ECommerceApp/Data/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using ECommerceApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceApp.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+
+        // The connection string for SQL Server database
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // You can replace the connection string below with your actual connection string
+            var connectionString = "Server=localhost;Database=EASV-DB-Compulsary-Assignment-1;User Id=sa;Password=YourStrong!Passw0rd;Encrypt=false;";
+            options.UseSqlServer(connectionString);
+        }
+    }
+}
